9f5bd13318e6c04d018eb1046c7e79839c1864f5
ShaharGdn <shahargadon@gmail.com>
2024-06-15 17:06:54 +0300

    Create a util service

--- services/util.service.js
+++ services/util.service.js
@@ -1,7 +1,16 @@
+import fs from 'fs'
+import fr from 'follow-redirects'
+
+const { http, https } = fr
+
 export const utilService = {
     makeId,
     makeLorem,
-    getRandomIntInclusive
+    getRandomIntInclusive,
+    readJsonFile,
+    writeJsonFile,
+    download,
+    httpGet,
 }
 
 function makeId(length = 6) {
@@ -29,4 +38,58 @@ function getRandomIntInclusive(min, max) {
     min = Math.ceil(min)
     max = Math.floor(max)
     return Math.floor(Math.random() * (max - min + 1)) + min //The maximum is inclusive and the minimum is inclusive 
+}
+function readJsonFile(path) {
+    const str = fs.readFileSync(path, 'utf8')
+    const json = JSON.parse(str)
+    return json
+}
+
+function writeJsonFile(path, data) {
+    return new Promise((resolve, reject) => {
+        const jsonData = JSON.stringify(data, null, 2)
+
+        fs.writeFile(path, jsonData, (err) => {
+            if (err) return reject(err)
+            resolve()
+        })
+    })
+}
+
+function download(url, fileName) {
+    return new Promise((resolve, reject) => {
+        const file = fs.createWriteStream(fileName)
+        https.get(url, (content) => {
+            content.pipe(file)
+            file.on('error', reject)
+            file.on('finish', () => {
+                file.close()
+                resolve()
+            })
+        })
+    })
+}
+
+function httpGet(url) {
+    const protocol = url.startsWith('https') ? https : http
+    const options = {
+        method: 'GET'
+    }
+
+    return new Promise((resolve, reject) => {
+        const req = protocol.request(url, options, (res) => {
+            let data = ''
+            res.on('data', (chunk) => {
+                data += chunk
+            })
+            res.on('end', () => {
+                resolve(data)
+            })
+        })
+        req.on('error', (err) => {
+            reject(err)
+        })
+        req.end()
+    })
+
 }
